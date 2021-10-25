e07d382f88f1679687b56a9b5bef0800fe945612
annzolotareva <92973848+annzolotareva@users.noreply.github.com>
2021-10-25 09:59:50 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.sliper-button-next',
+    prevEl: '.sliper-button-prev',
+  },
+});
